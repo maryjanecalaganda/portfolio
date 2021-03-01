e0aff279422507d8043acb80f2c3c9dfb1c88251
Darwin Camahalan <camahalandarwin@gmail.com>
2021-03-01 19:33:15 +0800

    Added Sections

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import './styles.scss'
+import './intro.scss'
+import './educ.scss'
+import './contact.scss'
+import './skills.scss'
 
 import profile from './assets/profile.jpg'
+import section3 from './assets/section 3.jpg'
+
+import { Parallax } from 'react-parallax'
 
 function App() {
   return (
-    <div className="main-app">
-      <div className="img">
-        <img src={profile} alt={profile} />
-        <div className="li">
-          <div className="ul">Introduction</div>
-          <div className="ul">Education</div>
-          <div className="ul">Contact</div>
-          <div className="ul">Skills</div>
+    <div>
+      <Parallax
+        className="main-app"
+        strength={300}
+        bgImage="https://c4.wallpaperflare.com/wallpaper/36/14/561/minimalism-minimalist-mountains-landscape-wallpaper-preview.jpg"
+      >
+        <div className="img">
+          <img src={profile} alt={profile} />
+          <div className="li">
+            <a href="#introduction" className="ul">
+              Introduction
+            </a>
+            <a href="#education" className="ul">
+              Education
+            </a>
+            <a href="#contact" className="ul">
+              Contact
+            </a>
+            <a href="#skills" className="ul">
+              Skills
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="name">
-        <h1>Mary Jane Calaganda</h1>
-      </div>
+        <div className="name">
+          <h1>Mary Jane Calaganda</h1>
+        </div>
+      </Parallax>
+      <section id="introduction" className="intro"></section>
+      <Parallax
+        strength={300}
+        bgImage="https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/4k/original/10.jpg"
+      >
+        <section id="education" className="educ"></section>
+      </Parallax>
+      <section id="skills" className="skills"></section>
+      <Parallax strength={300} bgImage={section3}>
+        <section id="contact" className="contact"></section>
+      </Parallax>
     </div>
   )
 }
