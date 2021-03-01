8ef9b2640f19077b1c811b8a020d8bcef4e2f814
Darwin Camahalan <camahalandarwin@gmail.com>
2021-03-01 17:30:05 +0800

    Enhance UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import './styles.scss'
+
+import profile from './assets/profile.jpg'
+
 function App() {
-  return <div>hello</div>
+  return (
+    <div className="main-app">
+      <div className="img">
+        <img src={profile} alt={profile} />
+        <div className="li">
+          <div className="ul">Introduction</div>
+          <div className="ul">Education</div>
+          <div className="ul">Contact</div>
+          <div className="ul">Skills</div>
+        </div>
+      </div>
+      <div className="name">
+        <h1>Mary Jane Calaganda</h1>
+      </div>
+    </div>
+  )
 }
 
 export default App
