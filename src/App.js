fd7c1bbfc942226fa5b331ce0a2c8111eaa0cdc5
Darwin Camahalan <camahalandarwin@gmail.com>
2021-03-02 15:29:44 +0800

    Added Animation

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import profile from './assets/profile.jpg'
 import section3 from './assets/section 3.jpg'
 import section2 from './assets/section 2.jpg'
 import section1 from './assets/section 1.jpg'
+import intropic from './assets/intro-pic.png'
 
 import { Parallax } from 'react-parallax'
-
+import { Link } from 'react-scroll'
 function App() {
   return (
     <div>
@@ -18,25 +19,61 @@ function App() {
         <div className="img">
           <img src={profile} alt={profile} />
           <div className="li">
-            <a href="#introduction" className="ul">
+            <Link
+              activeClass="active"
+              to="introduction"
+              smooth={true}
+              duration={500}
+              className="ul"
+            >
               Introduction
-            </a>
-            <a href="#education" className="ul">
+            </Link>
+            <Link
+              activeClass="active"
+              to="education"
+              smooth={true}
+              duration={600}
+              className="ul"
+            >
               Education
-            </a>
-            <a href="#contact" className="ul">
+            </Link>
+            <Link
+              activeClass="active"
+              to="contact"
+              smooth={true}
+              duration={800}
+              className="ul"
+            >
               Contact
-            </a>
-            <a href="#skills" className="ul">
+            </Link>
+            <Link
+              activeClass="active"
+              to="skills"
+              smooth={true}
+              duration={700}
+              className="ul"
+            >
               Skills
-            </a>
+            </Link>
           </div>
         </div>
         <div className="name">
           <h1>Mary Jane Calaganda</h1>
         </div>
       </Parallax>
-      <section id="introduction" className="intro"></section>
+      <section id="introduction" className="intro">
+        <img src={intropic} alt={intropic} />
+        <div className="me">
+          <h1>
+            Hi, I'm <span>Mary Jane C. Calaganda</span>{' '}
+          </h1>
+          <p>
+            I'm currently a TCM student at University of Science and Technology
+            of Southern Philippines.
+          </p>
+          <p>I'm 21 years old.</p>
+        </div>
+      </section>
       <Parallax strength={300} bgImage={section2}>
         <section id="education" className="educ"></section>
       </Parallax>
